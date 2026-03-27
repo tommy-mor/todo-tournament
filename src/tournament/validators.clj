a40b625ffa6edb6aa198891ffefaf72e9d947c33
tommy-mor <thmorriss@gmail.com>
2026-03-27 00:44:16 -0500

    nice

--- src/tournament/validators.clj
+++ src/tournament/validators.clj
@@ -1,72 +1,129 @@
 (ns tournament.validators
-  (:require [tommy-mor.trail :as t]))
+  (:require [tommy-mor.trail :as t]
+            [tournament.spel :as s]))
 
 ;; ---------------------------------------------------------------------------
-;; TODO-specific navigators (built on trail primitives)
+;; Validators are sequential scripts. Open, act, assert. Like Rama tests.
+;; Each one is (defn v-name [url] ...) that returns failures or nil.
 ;; ---------------------------------------------------------------------------
 
-(def todos       (t/role "listitem"))
-(def input       (t/role "textbox"))
-(def checkboxes  (t/role "checkbox"))
-(def buttons     (t/role "button"))
-(def heading     (t/role "heading"))
+(defn v-empty-state [url]
+  (s/open! url)
+  (let [els (s/snap-els!)]
+    (t/validate-all
+     [[(t/role "heading")]
+      [(t/role "textbox")]
+      [(t/absent (t/role "listitem"))]]
+     els)))
 
-(defn like [pat] (t/name-match pat))
-(def count-display (like #"\d+.*item"))
+(defn v-add-todo [url]
+  (s/open! url)
+  (let [input (s/find-ref [(t/role "textbox")])]
+    (s/fill! input "buy milk")
+    (s/press! "Enter")
+    (Thread/sleep 1000)
+    (let [els (s/snap-els!)]
+      (t/validate-all
+       [[(t/role "listitem")]
+        [(t/name-match #"buy milk")]
+        [(t/role "textbox")]]
+       els))))
 
-;; ---------------------------------------------------------------------------
-;; Specs: just paths. The trail IS the name.
-;; Use (t/label ...) only when intent isn't obvious from navigators alone.
-;; ---------------------------------------------------------------------------
+(defn v-add-three [url]
+  (s/open! url)
+  (doseq [text ["buy milk" "walk dog" "write code"]]
+    (let [input (s/find-ref [(t/role "textbox")])]
+      (s/fill! input text)
+      (s/press! "Enter")
+      (Thread/sleep 500)))
+  (let [els (s/snap-els!)]
+    (t/validate-all
+     [[(t/role "listitem") (t/count>= 3)]
+      [(t/name-match #"buy milk")]
+      [(t/name-match #"walk dog")]
+      [(t/name-match #"write code")]]
+     els)))
 
-(def empty-state-specs
-  [[(t/absent todos)]
-   [input]
-   [heading]])
+(defn v-complete-todo [url]
+  (s/open! url)
+  (let [input (s/find-ref [(t/role "textbox")])]
+    (s/fill! input "buy milk")
+    (s/press! "Enter")
+    (Thread/sleep 1000))
+  (let [cb (s/find-ref [(t/role "checkbox") t/unchecked])]
+    (s/click! cb)
+    (Thread/sleep 500))
+  (let [els (s/snap-els!)]
+    (t/validate-all
+     [[(t/role "checkbox") t/checked]]
+     els)))
 
-(def after-add-one-specs
-  [[todos (t/count= 1)]
-   [todos (t/name-match #"buy milk")]
-   [input]
-   [count-display]])
+(defn v-delete-todo [url]
+  (s/open! url)
+  (let [input (s/find-ref [(t/role "textbox")])]
+    (s/fill! input "buy milk")
+    (s/press! "Enter")
+    (Thread/sleep 1000))
+  (let [del (s/find-ref [(t/role "button") (t/name-match #"×|delete|remove")])]
+    (s/click! del)
+    (Thread/sleep 500))
+  (let [els (s/snap-els!)]
+    (t/validate-all
+     [[(t/absent (t/role "listitem"))]]
+     els)))
 
-(def after-add-three-specs
-  [[todos (t/count= 3)]
-   [(t/name= "buy milk")]
-   [(t/name= "walk dog")]
-   [(t/name= "write code")]])
+(defn v-count-display [url]
+  (s/open! url)
+  (doseq [text ["alpha" "beta"]]
+    (let [input (s/find-ref [(t/role "textbox")])]
+      (s/fill! input text)
+      (s/press! "Enter")
+      (Thread/sleep 500)))
+  (let [els (s/snap-els!)]
+    (t/validate-all
+     [[(t/name-match #"2.*item")]]
+     els)))
 
-(def after-complete-specs
-  [[checkboxes t/checked]
-   [checkboxes t/unchecked]
-   [count-display]])
-
-(def after-delete-specs
-  [[todos (t/count= 2)]])
-
-(def filter-specs
-  [[(like #"(?i)^all$")]
-   [(like #"(?i)^active$")]
-   [(like #"(?i)^completed$")]])
-
-(def clear-completed-specs
-  [[(like #"(?i)clear completed")]])
-
-(def xss-specs
-  [[(like #"<script>")]])
+(defn v-filter-buttons [url]
+  (s/open! url)
+  (let [input (s/find-ref [(t/role "textbox")])]
+    (s/fill! input "something")
+    (s/press! "Enter")
+    (Thread/sleep 1000))
+  (let [els (s/snap-els!)]
+    (t/validate-all
+     [[(t/name-match #"(?i)^all$")]
+      [(t/name-match #"(?i)^active$")]
+      [(t/name-match #"(?i)^completed$")]]
+     els)))
 
 ;; ---------------------------------------------------------------------------
 ;; Runner
 ;; ---------------------------------------------------------------------------
 
-(defn run-specs [specs elements]
-  (t/validate-all specs elements))
+(def all-validators
+  [["empty-state"    v-empty-state]
+   ["add-todo"       v-add-todo]
+   ["add-three"      v-add-three]
+   ["complete-todo"  v-complete-todo]
+   ["delete-todo"    v-delete-todo]
+   ["count-display"  v-count-display]
+   ["filter-buttons" v-filter-buttons]])
 
-(defn report [specs elements]
-  (let [failures (run-specs specs elements)]
-    (if (seq failures)
-      (do
-        (println (str (count failures) " of " (count specs) " failed:"))
-        (doseq [{:keys [fail message]} failures]
-          (println (str "  FAIL " (clojure.string/join " > " fail) ": " message))))
-      (println (str "All " (count specs) " specs pass.")))))
+(defn run-all! [url]
+  (println (str "Testing " url))
+  (println (apply str (repeat 50 "─")))
+  (doseq [[name validator] all-validators]
+    (print (str "  " name "... "))
+    (flush)
+    (try
+      (let [failures (validator url)]
+        (if (seq failures)
+          (do
+            (println "FAIL")
+            (doseq [{:keys [message]} failures]
+              (println (str "    " message))))
+          (println "ok")))
+      (catch Exception e
+        (println (str "ERROR: " (.getMessage e))))))
+  (println))
